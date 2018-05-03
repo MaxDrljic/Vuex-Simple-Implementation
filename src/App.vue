026336630019ad98f05299c260de3bff2e8bdd84
MaxDrljic <max_spree@hotmail.com>
2018-05-03 15:55:35 +0200

    Simple Vuex implementation.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
+  </div>
+</template>
+
+<script>
+import ProductListOne from './components/ProductListOne';
+import ProductListTwo from './components/ProductListTwo';
+
+export default {
+  components: {
+    ProductListOne,
+    ProductListTwo
+  },
+  name: 'app',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Ubuntu');
+
+body {
+  font-family: Ubuntu;
+  color: #555;
+}
+</style>
